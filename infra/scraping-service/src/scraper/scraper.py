8dbac43d9ec1c22298f4bbc667986c208c829ffc
Ziv <zivlazarov@gmail.com>
2025-06-21 17:13:25 +0300

    temp commit - rollback if needed

--- infra/scraping-service/src/scraper/scraper.py
+++ infra/scraping-service/src/scraper/scraper.py
@@ -1,13 +1,13 @@
-import requests
 from bs4 import BeautifulSoup
 import cloudscraper
-
 from io import TextIOWrapper
+import asyncio
+import aiohttp
+
+from .exceptions import HttpResponseException
 
 
-
-class TrackerGGScraper:
-    # https://tracker.gg/overwatch/leaderboards/stats/all/Eliminations?page=100&plat=mouseKeyboard&gamemode=competitive
+class TrackerGGScraper():
     def __init__(self, base_url='https://tracker.gg/overwatch/leaderboards/stats/all/Eliminations'):
         self.base_url = base_url
         self.scraper = cloudscraper.create_scraper(
@@ -18,10 +18,16 @@ class TrackerGGScraper:
                 'mobile': False
             }
         )
+
+        # self.session = aiohttp.ClientSession()
+
+    async def __del__(self):
+        await self.session.close()
         
     async def get_data_from_page(self, start_page: int, end_page: int, platform: str, game_mode: str, write_to_file: bool):
         # container to hold all the retrieved data
         data = []
+        futures = []
 
         # query params
         params = {
@@ -34,41 +40,55 @@ class TrackerGGScraper:
             with open('./users_test.txt', 'a') as f:
                 # traversing all pages and retrieving the data
                 for page in range(start_page, end_page + 1):
-                    await self._scrape_pages(
-                        current_page=page,
-                        query_params=params,
-                        data=data,
-                        write_to_file=write_to_file,
-                        file=f
-                    )
+                    try:
+                        futures.append(
+                            self._scrape_page(
+                                page=page,
+                                query_params=params,
+                                data=data,
+                                write_to_file=write_to_file,
+                                file=f
+                            )
+                        )
+                    except HttpResponseException as e:
+                        break
                 # sleeping to avoid IP block
                 # time.sleep(1)
         else:
             # not writing to file
             for page in range(start_page, end_page + 1):
-                await self._scrape_pages(
-                    current_page=page,
-                    query_params=params,
-                    data=data,
-                    write_to_file=write_to_file
-                )
+                try:
+                    futures.append(
+                        self._scrape_page(
+                            page=page,
+                            query_params=params,
+                            data=data,
+                            write_to_file=write_to_file
+                        )
+                    )
+                except HttpResponseException as e:
+                    break
+        # awaiting all calls together
+        await asyncio.gather(*futures)
 
         return data[:100]
     
-    async def _scrape_pages(
+    async def _scrape_page(
             self, 
-            current_page: int, 
+            page: int, 
             query_params: dict, 
             data: list, 
             write_to_file: bool, 
             file: TextIOWrapper=None
         ):
-        query_params['page'] = current_page
-        # retrieving the data
-        response = self.scraper.get(self.base_url, params=query_params)
+        query_params['page'] = page
+        try:
+            # retrieving the data
+            response = self.scraper.get(self.base_url, params=query_params)
+        except Exception:
+            raise HttpResponseException(f'Page {page} couldn\'t be scraped! Please check network configurations.')
         # only appending the data if request was successful
         if response.status_code == 200:
-            print(f'Appending data from page {current_page}...')
             soup = BeautifulSoup(response.text, 'html.parser')
             # extracting usernames and battle tags values from the html response
             usernames = soup.find_all('span', class_=['trn-ign__username', 'fit-long-username'])
@@ -83,6 +103,7 @@ class TrackerGGScraper:
                 if write_to_file:
                     file.write(f'username: {data[-1]['username']}\ntag: {data[-1]['tag']}\n\n')
         else:
-            print(f'Request failed on page {current_page}...')
+            raise Exception(f'Page {page} couldn\'t be scraped!')
+
        
 
