16e76c5236bbca6641df5a0058f389966d91f027
Ziv <zivlazarov@gmail.com>
2025-06-21 19:21:39 +0300

    scraping service: removed models directory and added models.py in scraper directory - added health endpoint and fixed healthcheck section in docker-compose.yaml

--- infra/scraping-service/src/main.py
+++ infra/scraping-service/src/main.py
@@ -5,3 +5,7 @@ from scraper.router import router as scraper_router
 app = FastAPI()
 
 app.include_router(scraper_router)
+
+@app.get('/health')
+def health():
+    return 200
--- infra/scraping-service/src/scraper/router.py
+++ infra/scraping-service/src/scraper/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Query
 from typing import Annotated
 
-from scraper.scraper import TrackerGGScraper
-from models.models import TrackerGGModel
+from .scraper import TrackerGGScraper
+from .models import TrackerGGModel
 
 
 router = APIRouter(
@@ -11,17 +11,6 @@ router = APIRouter(
 
 @router.get('')
 async def scrape_data(model: Annotated[TrackerGGModel, Query()]):
-    scraper = TrackerGGScraper()
-
-    return await scraper.get_data_from_page(
-        start_page=model.start_page,
-        end_page=model.end_page, 
-        platform=model.platform.value, 
-        game_mode=model.game_mode.value
-    )
-
-@router.post('')
-async def scrape_data_with_write(model: Annotated[TrackerGGModel, Query()]):
     scraper = TrackerGGScraper()
 
     return await scraper.get_data_from_page(
