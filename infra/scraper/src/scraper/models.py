6df681f86cb258bf295bad6b20db0c8fbbcf79a4
Ziv <zivlazarov@gmail.com>
2025-06-27 19:21:43 +0300

    scraper: added validation with start and end page values - end page must be greater or equal to start page

--- infra/scraper/src/scraper/models.py
+++ infra/scraper/src/scraper/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 from enum import StrEnum
 
 class TrackerGGPlatform(StrEnum):
@@ -15,3 +15,12 @@ class TrackerGGModel(BaseModel):
     end_page: int = Field(1, ge=1, le=9999)
     platform: TrackerGGPlatform = Field(TrackerGGPlatform.pc)
     game_mode: TrackerGGGameMode = Field(TrackerGGGameMode.competitive)
+
+    @model_validator(mode='after')
+    def validate_page_range(self) -> int:
+        start_page = self.start_page
+        end_page = self.end_page
+        if start_page > end_page:
+            raise ValueError(f'end_page must satisfy end_page <= start_page, but instead got start_page = {start_page}, end_page = {end_page}')
+        return self
+    
