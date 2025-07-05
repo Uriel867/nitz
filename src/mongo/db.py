98c54d91e7e0441029d17c926e1548b64722d1d4
uriel tcherbbis <uriel867@gmail.com>
2025-07-05 20:17:25 +0300

    implement new logic for collections added

--- src/mongo/db.py
+++ src/mongo/db.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 
 _client = MongoClient("mongodb://localhost:27017/")
-_db = _client["testdb"]
-collection = _db["summoners"]
+_db = _client["lol"]
+summoners_collection = _db["summoners"]
+match_id_collection = _db["match_id"]
+match_data_collection = _db["match_data"]
--- src/mongo/service.py
+++ src/mongo/service.py
@@ -1,23 +1,23 @@
-from mongo.db import collection
+from mongo.db import summoners_collection, match_id_collection,match_data_collection
 
 class MongoService():
     def insert_summoner(self,summoner_data: dict):
         try:
-            result = collection.insert_one(summoner_data)
+            result = summoners_collection.insert_one(summoner_data)
             return {"message": f"Inserted document with ID: {str(result.inserted_id)}", "success": True}
         except Exception as e:
             return {"error": f"An error occurred while inserting data: {e}", "success": False}
         
     def insert_many_summoners(self, summoner_data: list):
         try:
-            summoners = collection.insert_many(summoner_data)
+            summoners = summoners_collection.insert_many(summoner_data)
             return {"message": f"Inserted {len(summoners.inserted_ids)} documents", "success": True}
         except Exception as e:
             return {"error": f"An error occurred while inserting data: {e}", "success": False}
         
     def get_all_summoners(self):
         try:
-            summoners = list(collection.find({}, {"_id": 0}))  # Exclude the MongoDB ObjectId from the results
+            summoners = list(summoners_collection.find({}, {"_id": 0}))  # Exclude the MongoDB ObjectId from the results
             if not summoners:
                 return {"message": "No summoners found", "success": True}   
             return summoners
@@ -26,9 +26,44 @@ class MongoService():
         
     def get_summoner(self, summoner_name: str, battle_tag: int):
         try:
-            summoner = collection.find_one({"summoner_name": summoner_name, "battle_tag": battle_tag}, {"_id": 0})  # Exclude the MongoDB ObjectId from the results
+            summoner = summoners_collection.find_one({"summoner_name": summoner_name, "battle_tag": battle_tag}, {"_id": 0})  # Exclude the MongoDB ObjectId from the results
             if not summoner:
                 return {"message": "Summoner not found", "success": True}
             return summoner
         except Exception as e:
             return {"error": f"An error occurred while fetching the summoner: {e}", "success": False}
+
+    def get_match_id(self, match_id: str):
+        try:
+            match = match_id_collection.find_one({"match_id": match_id}, {"_id": 0})  # Exclude the MongoDB ObjectId from the results
+            if not match:
+                return {"message": "Match not found", "success": True}
+            return match
+        except Exception as e:
+            return {"error": f"An error occurred while fetching match IDs: {e}", "success": False}
+        
+    def insert_match_id(self, match_id: dict):
+        try:
+            result = match_id_collection.insert_one(match_id)
+            return {"message": f"Inserted match document with ID: {str(result.inserted_id)}", "success": True}
+        except Exception as e:
+            return {"error": f"An error occurred while inserting match data: {e}", "success": False}
+        
+    def get_match_data_by_summoner(self, summoner_puuid: str):
+        try:
+            match_data = match_data_collection.find({"summoner_name": summoner_puuid}, {"_id": 0})  # Exclude the MongoDB ObjectId from the results
+            matches = list(match_data)
+            if not matches:
+                return {"message": "No matches found for this summoner", "success": True}
+            return matches
+        except Exception as e:
+            return {"error": f"An error occurred while fetching match data: {e}", "success": False}
+    
+    def get_match_data_by_match_id(self,match_id):
+        try:
+            match_data = match_data_collection.find_one({"match id": match_id}, {"_id": 0})
+            if not match_data:
+                return {"message": "match data not found", "success":True}
+            return match_data
+        except Exception as e:
+            return {"error": f"An error occurred while fetching match data: {e}", "success": False} 
