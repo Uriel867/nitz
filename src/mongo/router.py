211b5bf587ef36dc82db9c4544ea65a7ebd78562
uriel tcherbbis <uriel867@gmail.com>
2025-07-05 19:02:37 +0300

    changed users to summoners according to lol vocabulary

--- src/mongo/router.py
+++ src/mongo/router.py
@@ -3,46 +3,46 @@ from fastapi import FastAPI,APIRouter
 from typing import List, Dict
 from mongo.service import MongoService
 
-router = APIRouter(prefix="/users")
+router = APIRouter(prefix="/summoners")
 ms = MongoService()
 
 @router.post("/insert")  
-def insert_user(user_data: dict):
+def insert_summoner(summoner_data: dict):
     """
-    Inserts user data directly into the MongoDB collection.
+    Inserts summoner data directly into the MongoDB collection.
     
-    :param user_data: User data to be inserted (received from API request)
+    :param summoner_data: Summoner data to be inserted (received from API request)
     """
     
-    return ms.insert_user(user_data)
+    return ms.insert_summoner(summoner_data)
 
 @router.post("/insert_many")
-def insert_many_users(user_data: List[Dict]):
+def insert_many_summoners(summoner_data: List[Dict]):
     """
-    Inserts multiple user data into the MongoDB collection.
+    Inserts multiple summoner data into the MongoDB collection.
     
-    :param user_data: List of user data to be inserted (received from API request)
+    :param summoner_data: List of summoner data to be inserted (received from API request)
     """
     
-    return ms.insert_many_users(user_data)
+    return ms.insert_many_summoners(summoner_data)
 
 @router.get("/all")
-def get_all_users():
+def get_all_summoners():
     """
-    Retrieves all users from the MongoDB collection.
+    Retrieves all summoners from the MongoDB collection.
     
-    :return: List of all users or a message if no users are found
+    :return: List of all summoners or a message if no summoners are found
     """
     
-    return ms.get_all_users()
+    return ms.get_all_summoners()
 
 @router.get("/get")
-def get_user(user_name: str,battle_tag: int):
+def get_summoner(summoner_name: str,battle_tag: int):
     """
-    Retrieves a user from the MongoDB collection based on user_name and battle_tag.
+    Retrieves a summoner from the MongoDB collection based on summoner_name and battle_tag.
     
-    :param user_name: The name of the user to retrieve
-    :param battle_tag: The battle tag of the user to retrieve
-    :return: The user data or a message if the user is not found
+    :param summoner_name: The name of the summoner to retrieve
+    :param battle_tag: The battle tag of the summoner to retrieve
+    :return: The summoner data or a message if the summoner is not found
     """
-    return ms.get_user(user_name, battle_tag)
+    return ms.get_summoner(summoner_name, battle_tag)
--- src/mongo/service.py
+++ src/mongo/service.py
@@ -1,34 +1,34 @@
 from mongo.db import collection
 
 class MongoService():
-    def insert_user(self,user_data: dict):
+    def insert_summoner(self,summoner_data: dict):
         try:
-            result = collection.insert_one(user_data)
+            result = collection.insert_one(summoner_data)
             return {"message": f"Inserted document with ID: {str(result.inserted_id)}", "success": True}
         except Exception as e:
             return {"error": f"An error occurred while inserting data: {e}", "success": False}
         
-    def insert_many_users(self, user_data: list):
+    def insert_many_summoners(self, summoner_data: list):
         try:
-            users = collection.insert_many(user_data)
-            return {"message": f"Inserted {len(users.inserted_ids)} documents", "success": True}
+            summoners = collection.insert_many(summoner_data)
+            return {"message": f"Inserted {len(summoners.inserted_ids)} documents", "success": True}
         except Exception as e:
             return {"error": f"An error occurred while inserting data: {e}", "success": False}
         
-    def get_all_users(self):
+    def get_all_summoners(self):
         try:
-            users = list(collection.find({}, {"_id": 0}))  # Exclude the MongoDB ObjectId from the results
-            if not users:
-                return {"message": "No users found", "success": True}   
-            return users
+            summoners = list(collection.find({}, {"_id": 0}))  # Exclude the MongoDB ObjectId from the results
+            if not summoners:
+                return {"message": "No summoners found", "success": True}   
+            return summoners
         except Exception as e:
-            return {"error": f"An error occurred while fetching users: {e}", "success": False}
+            return {"error": f"An error occurred while fetching summoners: {e}", "success": False}
         
-    def get_user(self, user_name: str, battle_tag: int):
+    def get_summoner(self, summoner_name: str, battle_tag: int):
         try:
-            user = collection.find_one({"user_name": user_name, "battle_tag": battle_tag}, {"_id": 0})  # Exclude the MongoDB ObjectId from the results
-            if not user:
-                return {"message": "User not found", "success": True}
-            return user
+            summoner = collection.find_one({"summoner_name": summoner_name, "battle_tag": battle_tag}, {"_id": 0})  # Exclude the MongoDB ObjectId from the results
+            if not summoner:
+                return {"message": "Summoner not found", "success": True}
+            return summoner
         except Exception as e:
-            return {"error": f"An error occurred while fetching the user: {e}", "success": False}
+            return {"error": f"An error occurred while fetching the summoner: {e}", "success": False}
