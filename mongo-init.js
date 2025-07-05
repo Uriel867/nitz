2e19548e8080de6f00e65a37a608b7e137070054
uriel tcherbbis <uriel867@gmail.com>
2025-07-05 19:02:37 +0300

    changed users to summoners according to lol vocabulary

--- mongo-init.js
+++ mongo-init.js
@@ -1,5 +1,5 @@
 db = db.getSiblingDB('testdb');
-db.createCollection('users');
-db.users.insertOne({ name: "Alice", age: 30, email: "alice@example.com" });
-db.users.insertOne({ name: "Bob", age: 25, email: "bob@example.com" });
+db.createCollection('summoners');
+db.createCollection('match');
+db.createCollection('matchID');
 console.log("Database and collection initialized successfully!");
--- src/mongo/db.py
+++ src/mongo/db.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 
 _client = MongoClient("mongodb://localhost:27017/")
 _db = _client["testdb"]
-collection = _db["users"]
+collection = _db["summoners"]
