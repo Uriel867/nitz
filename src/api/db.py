a8b311498865dcb8ae9c46e597a8994040ff679f
uriel tcherbbis <uriel867@gmail.com>
2025-07-07 12:18:39 +0300

    removed pycache from tracking added .venv directory to gitignore

--- src/api/db.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-_client = MongoClient("mongodb://localhost:27017/")
-_db = _client["lol"]
-summoners_collection = _db["summoners"]
-match_id_collection = _db["match_id"]
-match_data_collection = _db["match_data"]
