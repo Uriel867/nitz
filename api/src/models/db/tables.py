ace38591c502355b1c49de8ab0915f0334ea2466
Ziv Lazarov <zivlazarov@gmail.com>
2025-07-26 11:34:39 +0300

    postgres: added a tables file under models directory - removed unnecessary api calls from scraper

---
+++ api/src/models/db/tables.py
@@ -0,0 +1,3 @@
+from sqlalchemy import Table, Column, Integer, String, Boolean
+
+# TODO: add all relevant tables as classes here
--- api/src/scraper/router.py
+++ api/src/scraper/router.py
@@ -22,38 +22,3 @@ async def scrape_data(model: Annotated[LeagueOfGraphsModel, Query()], scraper: S
         end_page=model.end_page,
         region=model.region
     )
-
-@router.get('/lol')
-async def get_puuid_by_summoner_name_and_tag(
-    summoner_name: str, 
-    summoner_tag: str, 
-    table_name: str, 
-    engine: Engine = Depends(provide_postgres_engine)
-):
-    import requests
-    import os
-    import json
-
-    headers = {
-        "Origin": "https://developer.riotgames.com",
-        "X-Riot-Token": os.environ['RIOT_GAMES_API_KEY']
-    }
-    
-    url = f'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{summoner_name}/{summoner_tag}'
-
-    response = requests.get(url=url, headers=headers)
-
-    json_data = json.loads(response.content)
-
-    with engine.begin() as conn:
-        conn.execute(text(f'CREATE TABLE {table_name} (x int, y int)'))
-        conn.execute(
-            text(f'INSERT INTO {table_name} (x, y) VALUES (:x, :y)'),
-            [{'x': 1, 'y': 16}, {'x': 13, 'y': 27}]
-        )
-
-        result = conn.execute(text(f'SELECT x AS ziv, y AS lazarov FROM {table_name}'))
-        for row in result:
-            print(f'x: {row.ziv}, y: {row.lazarov}')
-    
-    return json_data
