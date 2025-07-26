771b912ea0f4f6ea439380cae25308b7cede5172
Ziv Lazarov <zivlazarov@gmail.com>
2025-07-26 11:32:49 +0300

    posgres: added sqlalchemy metadata provider function - changed engine global variable name to be more intuitive for its functionality

--- api/src/di/dependencies.py
+++ api/src/di/dependencies.py
@@ -2,30 +2,34 @@ from typing import Annotated
 from fastapi import Depends
 from pymongo import MongoClient
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 
 from reporter.service import LoLStatsService
 from scraper.service import LeagueOfGraphsScraperService
 
 
-engine = None
-scraper_service = None
+_postgres_engine = None
+_postgres_metadata = None
 
 def provide_scraper_service():
-    global scraper_service
-    if not scraper_service:
-        scraper_service = LeagueOfGraphsScraperService()
+    scraper_service = LeagueOfGraphsScraperService()
 
     yield scraper_service
     # anything that needs to be executed after the function that's being injected with this dependency should go here
     # for example, closing a db connection
 
+def provide_postgres_metadata():
+    global _postgres_metadata
+    if not _postgres_metadata:
+        _postgres_metadata = MetaData()
+    return _postgres_metadata
+
 def provide_postgres_engine():
-    global engine
-    if not engine:
-        engine = create_engine(os.environ['POSTGRES_DB_URL'], echo=True)
+    global _postgres_engine
+    if not _postgres_engine:
+        _postgres_engine = create_engine(os.environ['POSTGRES_DB_URL'], echo=True)
     
-    yield engine
+    yield _postgres_engine
 
 def provide_mongo_client():
     mongo_client = MongoClient(host="mongodb")
