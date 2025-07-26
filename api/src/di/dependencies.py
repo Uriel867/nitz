445568de0528b891b4fe3c20542eaa5bc1344b50
Ziv Lazarov <zivlazarov@gmail.com>
2025-07-26 11:32:49 +0300

    posgres: added sqlalchemy metadata provider function - changed engine global variable name to be more intuitive for its functionality

--- api/src/di/dependencies.py
+++ api/src/di/dependencies.py
@@ -4,8 +4,10 @@ from reporter.service import LoLStatsService
 from pymongo import MongoClient
 from traffic_managment.async_leaky_bucket import AsyncLeakyBucket
 from riot_games.service import RiotGamesService
-import asyncio
 import os
+from sqlalchemy import create_engine, MetaData
+from reporter.service import LoLStatsService
+from scraper.service import LeagueOfGraphsScraperService
 
 #Buckets
 account_by_id_limiter = AsyncLeakyBucket(capacity=1000,leak_rate=1000/60) # 2000 requests every 1 minute
@@ -44,4 +46,40 @@ async def acquire_account_by_id_limiter(request: Request):
 async def acquire_match_by_match_id_limiter(request: Request):
     await match_by_id_limiter.acquire()
 
+def provide_scraper():
+    scraper = LeagueOfGraphsScraperService()
+    
+scraper_service = None
+_postgres_engine = None
+_postgres_metadata = None
 
+def provide_scraper_service():
+    scraper_service = LeagueOfGraphsScraperService()
+
+    yield scraper_service
+    # anything that needs to be executed after the function that's being injected with this dependency should go here
+    # for example, closing a db connection
+
+def provide_postgres_metadata():
+    global _postgres_metadata
+    if not _postgres_metadata:
+        _postgres_metadata = MetaData()
+    return _postgres_metadata
+
+def provide_postgres_engine():
+    global _postgres_engine
+    if not _postgres_engine:
+        _postgres_engine = create_engine(os.environ['POSTGRES_DB_URL'], echo=True)
+    
+    yield _postgres_engine
+
+def provide_mongo_client():
+    mongo_client = MongoClient(host="mongodb")
+    
+    yield mongo_client
+    
+# Dependency function for LoLStatsService
+def provide_lol_stats_service(
+    mongo_client: Annotated[MongoClient, Depends(provide_mongo_client)]
+) -> LoLStatsService:
+    return LoLStatsService(mongo_client)
