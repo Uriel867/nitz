d6df669fdf189e9989b5436ae13ef352c79c7faf
Ziv Lazarov <zivlazarov@gmail.com>
2025-09-04 12:18:16 +0300

    scraper: moved scraper service from infra directory to api directory - will be used as a standard api endpoint - reducing overhead of maintaining similar applications that are coupled

--- api/src/di/dependencies.py
+++ api/src/di/dependencies.py
@@ -1,38 +1,43 @@
+# external imports
 from typing import Annotated
 from fastapi import Depends, Request
 from reporter.service import LoLStatsService
 from pymongo import MongoClient
+import os
+# internal imports
+from scraper.service import LeagueOfGraphsScraper
 from traffic_managment.async_leaky_bucket import AsyncLeakyBucket
 from riot_games.service import RiotGamesService
-import asyncio
-import os
 
 #Buckets
 account_by_id_limiter = AsyncLeakyBucket(capacity=1000,leak_rate=1000/60) # 2000 requests every 1 minute
 account_by_puuid_limiter = AsyncLeakyBucket(capacity=1000,leak_rate=1000/60) # 1000 requests every 1 minute
 match_by_id_limiter = AsyncLeakyBucket(2000,2000/10) # 2000 requests every 10 seconds
 
-riot_games_service = RiotGamesService(os.getenv("RIOT_API_KEY"))
+riot_games_service = RiotGamesService(os.getenv("RIOT_GAMES_API_KEY"))
 
 #Dependencies for LolStatsService
 def provide_mongo_client():
-    mongo_host = os.getenv("MONGO_HOST")
-    mongo_client = MongoClient(host=mongo_host)
-    
+    mongo_client = MongoClient(host=os.getenv("MONGODB_URL"))
     try:
         yield mongo_client
-    
     finally:
         mongo_client.close()
     
 def provide_lol_stats_service(
-    mongo_client: Annotated[MongoClient, Depends(provide_mongo_client)]) -> LoLStatsService:
+        mongo_client: Annotated[MongoClient, Depends(provide_mongo_client)]
+) -> LoLStatsService:
     return LoLStatsService(mongo_client)
 
 #Dependencies for RiotGameService
 def provide_riot_games_service():
     yield riot_games_service
 
+def provide_scraper():
+    scraper = LeagueOfGraphsScraper()
+    yield scraper
+    # anything that needs to be executed after the function that's being injected with this dependency should go here
+    # for example, closing a db connection
 
 #Dependency for RiotGames
 async def acquire_account_puuid_limiter(request: Request):
--- api/src/main.py
+++ api/src/main.py
@@ -1,10 +1,16 @@
-from fastapi import  FastAPI
+from fastapi import FastAPI
 from reporter.router import router as reporter_router
 from riot_games.router import router as riot_games_router
+from scraper.router import router as scraper_router
 
 app = FastAPI()
 
 app.include_router(reporter_router)
 app.include_router(riot_games_router)
+app.include_router(scraper_router)
+
+@app.get('/health')
+def health():
+    return 200
 
 
