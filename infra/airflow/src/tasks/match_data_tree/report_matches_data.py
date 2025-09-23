7f4177b99241280dde5126f33607135df2bc7ddc
uriel tcherbbis <uriel867@gmail.com>
2025-09-23 13:23:20 +0300

    made fetch and report matches async

--- infra/airflow/src/tasks/match_data_tree/report_matches_data.py
+++ infra/airflow/src/tasks/match_data_tree/report_matches_data.py
@@ -1,18 +1,24 @@
 from airflow.operators.python import get_current_context
 from utils.exceptions import request_with_handle
 import os
+import asyncio
 
-#iterate through the list of matches and report each one
-async def report_matches_to_mongo():
+def report_matches_to_mongo():
     current_task = get_current_context()['ti']
     matches_ids = current_task.xcom_pull(task_ids='match_tree')
+    asyncio.run(fetch_and_report_all_matches(matches_ids))
 
-    for match_id in matches_ids:
-        match_data = await get_match_data(match_id)
-        await report_match(match_id, match_data)
+async def fetch_and_report_all_matches(matches_ids):
+    fetch_and_report = [fetch_and_report_match(match_id) for match_id in matches_ids]
+    await asyncio.gather(*fetch_and_report)
+
+#iterate through the list of matches and report each one
+async def fetch_and_report_match(match_id):
+    match_data = await fetch_match_data(match_id)
+    await report_match(match_id, match_data)
 
 
-async def get_match_data(match_id: str):
+async def fetch_match_data(match_id: str):
     return request_with_handle('GET',f'{os.getenv('NITZ_API_URL')}/match/by-match-id/{match_id}')
 
 
