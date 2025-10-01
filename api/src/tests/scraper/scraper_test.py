140bf0ed558404abcdb53204551a29bbdb64b18f
Ziv Lazarov <zivlazarov@gmail.com>
2025-10-01 15:37:38 +0300

    changed scraping test parameter to sub_region instead of region to keep project's conventions

--- api/src/tests/scraper/scraper_test.py
+++ api/src/tests/scraper/scraper_test.py
@@ -7,15 +7,15 @@ from main import app
 def client():
     yield TestClient(app)
 
-@pytest.mark.parametrize('start_page, end_page, region', [
+@pytest.mark.parametrize('start_page, end_page, sub_region', [
     (1, 1, 'eune'),
     (1, 1, 'euw')
 ])
-def test_scrape_endpoint(client: TestClient, start_page: int, end_page: int, region: str):
+def test_scrape_endpoint(client: TestClient, start_page: int, end_page: int, sub_region: str):
     query_params = {
         'start_page': start_page,
         'end_page': end_page,
-        'region': region
+        'sub_region': sub_region
     }
     response = client.get('/scrape', params=query_params)
 
