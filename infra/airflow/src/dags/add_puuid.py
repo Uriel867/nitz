382a0cd529f7c201305cc3ab03148f921bfb9f5a
uriel tcherbbis <uriel867@gmail.com>
2025-12-01 11:45:25 +0200

    new dag to add puuids to all summoners in collection

---
+++ infra/airflow/src/dags/add_puuid.py
@@ -0,0 +1,53 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from dags import default_args
+from utils.make_chunks import make_chunks_task
+from utils.report_data import fetch_and_report_chunk_task
+from utils.fetch_all_summoners import fetch_all_summoners_task
+import os
+
+def triggerer():
+    return True
+
+
+with DAG(
+        dag_id='add_puuid',
+        default_args=default_args,
+        catchup=False
+) as dag:
+
+    triggerer_task = PythonOperator(
+        task_id='triggerer',
+        python_callable=triggerer
+    )
+
+    # scrape for each region
+    fetch_all_summoners_task = PythonOperator(
+        task_id='fetch_all_summoners',
+        python_callable=fetch_all_summoners_task
+    )
+
+    matches_ids_chunks_task = PythonOperator(
+        task_id=f'make_chunks',
+        python_callable=make_chunks_task,
+        op_kwargs={'task_id': 'fetch_all_summoners',
+                   'chunk_size': 10},
+    )
+
+    report_chunks_task = PythonOperator.partial(
+        task_id=f'report_chunk',
+        python_callable=fetch_and_report_chunk_task,
+    ).expand(
+        op_kwargs=matches_ids_chunks_task.output.map(
+            lambda chunk: {
+                "chunk": chunk,
+                "fetch_url": f'{os.getenv("NITZ_API_URL")}/account/by-id',
+                "report_url": f'{os.getenv("NITZ_API_URL")}/reporter/summoner/add-puuid',
+                "summoner": True,
+                "match": False,
+            }
+        )
+    )
+
+
+    triggerer_task >> fetch_all_summoners_task >> matches_ids_chunks_task >> report_chunks_task
