96f1cf457f1c23bd3311188bc7d566d116f95f42
uriel tcherbbis <uriel867@gmail.com>
2025-12-16 11:38:38 +0200

    fixed match tree dag to fetch by region

--- infra/airflow/src/tasks/match_data_tree/fetch_matches_ids.py
+++ infra/airflow/src/tasks/match_data_tree/fetch_matches_ids.py
@@ -29,53 +29,59 @@ async def fetch_first_summoner_puuid(summoners: List[str]):
 
     puuid = await fetch_puuid(tag_line, summoner_name, region)
 
-    return puuid
+    return {"puuid": puuid, "region": region}
 
 def fetch_first_summoner_matches_task():
     current_task = get_current_context()['ti']  # ti - current task instance
-    root_puuid = current_task.xcom_pull(task_ids='fetch_first_summoner_puuid')
+    first_summoner = current_task.xcom_pull(task_ids='fetch_first_summoner_puuid')
+    puuid = first_summoner['puuid']
+    region = first_summoner['region']
 
-    return asyncio.run(fetch_first_summoner_matches(root_puuid))
+    return asyncio.run(fetch_first_summoner_matches(puuid, region))
 
-async def fetch_first_summoner_matches(puuid: str):
-    return await request_with_handle(method='GET', url=f'{os.getenv("NITZ_API_URL")}/match/by-puuid/{puuid}')
+async def fetch_first_summoner_matches(puuid: str, region: str):
+    return await request_with_handle(method='GET', url=f'{os.getenv("NITZ_API_URL")}/match/by-puuid/{puuid}', params={'region': region})
 
 
-# returns the the participants puuids of a match
-async def fetch_match_participants(match_id: str):
-    match_data = await request_with_handle(method='GET', url=f'{os.getenv("NITZ_API_URL")}/match/by-match-id/{match_id}')
+# returns the participants puuids of a match
+async def fetch_match_participants(match_id: str, region: str):
+    match_data = await request_with_handle(method='GET', url=f'{os.getenv("NITZ_API_URL")}/match/by-match-id/{match_id}', params={'region': region})
     match_participants = match_data['metadata']['participants']
 
     return match_participants
 
 def fetch_matches_ids_task(depth: int):
-    return asyncio.run(fetch_matches_ids(depth))
+    current_task = get_current_context()['ti']  # ti - current task instance
+    first_summoner = current_task.xcom_pull(task_ids='fetch_first_summoner_puuid')
+    region = first_summoner['region']
+
+    return asyncio.run(fetch_matches_ids(depth, region))
 
 
 # gathers matches ids by collecting matches from different summoners
-async def fetch_matches_ids(depth: int):
+async def fetch_matches_ids(depth: int, region: str):
     current_task = get_current_context()['ti']  # ti - current task instance
     matches_ids = current_task.xcom_pull(task_ids='fetch_first_summoner_matches')
     root_puuid = current_task.xcom_pull(task_ids='fetch_first_summoner_puuid') # starting from this summoner
 
     matches_participants = []
-    seen_puuids = {root_puuid}
+    seen_puuids = {root_puuid["puuid"]}
     seen_matches = set()
     match_ids_index  = 0 # tracks length of matches_ids
     matches_participants_index = 0 # tracks length of matches_participants
 
     # how deep we want the matches ids "tree" to be
     while depth > 0:
-        match_ids_index = await fetch_puuids_from_matches(matches_ids, seen_puuids, matches_participants, match_ids_index)
-        matches_participants_index = await fetch_matches_ids_from_participants(matches_ids, matches_participants, seen_matches, matches_participants_index)
+        match_ids_index = await fetch_puuids_from_matches(matches_ids, seen_puuids, matches_participants, match_ids_index, region)
+        matches_participants_index = await fetch_matches_ids_from_participants(matches_ids, matches_participants, seen_matches, matches_participants_index, region)
         depth -= 1
 
     return matches_ids
 
 # iterates over all matches we have and extracts participants from each one
-async def fetch_puuids_from_matches(matches_ids: List[str], seen_puuids: set[str], matches_participants: List[str], index: int):
+async def fetch_puuids_from_matches(matches_ids: List[str], seen_puuids: set[str], matches_participants: List[str], index: int, region: str):
     while index < len(matches_ids):
-        for puuid in await fetch_match_participants(matches_ids[index]):
+        for puuid in await fetch_match_participants(matches_ids[index], region):
             if puuid not in seen_puuids:
                 seen_puuids.add(puuid)
                 matches_participants.append(puuid)
@@ -83,9 +89,9 @@ async def fetch_puuids_from_matches(matches_ids: List[str], seen_puuids: set[str
     return index
 
 # iterates over the participants we gathered and extracts their latest matches ids
-async def fetch_matches_ids_from_participants(matches_ids: List[str], matches_participants: List[str], seen_matches: set[str], index: int):
+async def fetch_matches_ids_from_participants(matches_ids: List[str], matches_participants: List[str], seen_matches: set[str], index: int, region):
     while index < len(matches_participants):
-        for match_id in await fetch_first_summoner_matches(matches_participants[index]):
+        for match_id in await fetch_first_summoner_matches(matches_participants[index], region):
             if match_id not in seen_matches and '_' in match_id:
                 seen_matches.add(match_id)
                 matches_ids.append(match_id)
